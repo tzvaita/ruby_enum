0968189063c37b0b180604e74dcf429da9f83aaa
tzvaita <tzvaita@gmail.com>
2020-01-24 22:08:16 +0200

    finished my_select

--- enum.rb
+++ enum.rb
@@ -26,3 +26,17 @@ def my_each_with_index
     end
     self
   end
+
+  def my_select
+    return to_enum(:my_each) if !block_given?
+
+    arr = []
+    x = 0
+    while x < self.length
+      if yield(self[x])
+        arr << self[x]
+      end
+      x += 1
+    end
+    arr
+  end
