28094e60596ff13edca5d30b3821ca01ef1959d1
tzvaita <tzvaita@gmail.com>
2020-01-24 22:20:32 +0200

    all, any and none methods done

--- enum.rb
+++ enum.rb
@@ -14,29 +14,67 @@ module Enumerable
         self
 
     end
+
+
+    def my_each_with_index
+        return to_enum unless block_given?
+
+        x = 0
+        while x < self.length
+        yield(self[x], x)
+        x += 1
+        end
+        self
+    end
+
+    def my_select
+        return to_enum(:my_each) if !block_given?
+
+        arr = []
+        x = 0
+        while x < self.length
+        if yield(self[x])
+            arr << self[x]
+        end
+        x += 1
+        end
+        arr
+    end
+
+    def my_all?(pat = nil)
+        result = true
+        if block_given?
+        my_each { |ele| result &= (yield ele) }
+        elsif pat
+        my_each { |ele| result &= pat === ele }
+        else
+        my_each { |ele| result &= ele }
+        end
+        result
+    end
+
+    def my_any?(pat = nil)
+        result = false
+        if block_given?
+        my_each { |ele| result = true if yield ele }
+        elsif pat
+        my_each { |ele| result = true if pattern?(ele, pat) }
+        else
+        my_each { |ele| result = true if ele }
+        end
+        result
+    end
+
+    def my_none?(pat = nil)
+        result = true
+        if block_given?
+        my_each { |ele| result = false if yield ele }
+        elsif pat
+        my_each { |ele| result = false if pattern?(ele, pat) }
+        else
+        my_each { |ele| result = false if ele }
+        end
+        result
+    end
+
 end
-
-def my_each_with_index
-    return to_enum unless block_given?
-
-    x = 0
-    while x < self.length
-      yield(self[x], x)
-      x += 1
-    end
-    self
-  end
-
-  def my_select
-    return to_enum(:my_each) if !block_given?
-
-    arr = []
-    x = 0
-    while x < self.length
-      if yield(self[x])
-        arr << self[x]
-      end
-      x += 1
-    end
-    arr
-  end
